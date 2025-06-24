d85bf2bb137afc3064502527ef29ece63b9dc29e
deelicieuse <libatiqueangelique@gmail.com>
2025-06-25 01:32:20 +0800

    intializing class BankGui

--- gui/bank_gui.py
+++ gui/bank_gui.py
@@ -5,3 +5,12 @@ from bank_core.balance_exception import BalanceException
 from bank_core.bank_accounts import BankAccount
 from bank_core.interest_rewards_acct import InterestRewardsAcct
 from bank_core.savings_acct import SavingsAcct
+
+class BankGui:
+    def __init__(self, root):
+        self.root = root
+        root.title("Bank ATM Simulation")
+        root.geometry("550x580")
+        root.resizeable(False,False)
+
+        pass
