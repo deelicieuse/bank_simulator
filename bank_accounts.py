90a9dac93d6f2253126d4a4957f8acf32e9c7cb2
deelicieuse <libatiqueangelique@gmail.com>
2025-06-25 00:11:48 +0800

    forgot to commit: added methods getBalance, deposit, and empty method withdraw (will update names later)

--- bank_accounts.py
+++ bank_accounts.py
@@ -2,5 +2,17 @@ class BankAccount:
     def __init__(self, intialAmount, acctName):
         self.balance = intialAmount
         self.name = acctName
-        print(f"\nAccount '{self.name} created.\nBalance = {self.
-              balance:.2f}")
+        print(f"\nAccount '{self.name}' created."
+              f"\nBalance = {self.balance:.2f}")
+
+    def getBalance(self):
+        print(f"\nAccount: '{self.name}'"
+              f"\nBalance = ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("\nDeposit Complete")
+        self.getBalance()
+
+    def withdraw(self):
+        pass
---
+++ tester.py
@@ -0,0 +1,8 @@
+from bank_accounts import *
+
+Dave = BankAccount(10000, "Dave")
+Sara = BankAccount(2000, "Sara")
+
+Dave.getBalance()
+
+Sara.deposit(500)
