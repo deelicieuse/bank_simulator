e9aed260a926c7a6d590893268860f63c07f088b
deelicieuse <libatiqueangelique@gmail.com>
2025-06-25 01:23:48 +0800

    added bank_gui.py + imported modules

---
+++ gui/bank_gui.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog, ttk
+
+from bank_core.balance_exception import BalanceException
+from bank_core.bank_accounts import BankAccount
+from bank_core.interest_rewards_acct import InterestRewardsAcct
+from bank_core.savings_acct import SavingsAcct
